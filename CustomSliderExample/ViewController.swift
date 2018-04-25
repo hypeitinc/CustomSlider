a86c0f629250bd53b3339d767ca4c3b890f4af75
Peter Rexelius <peter@hypeit.net>
2018-04-25 15:22:20 -0700

    Initial Commit

---
+++ CustomSliderExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSliderExample
+//
+//  Created by Peter Rexelius on 4/25/18.
+//  Copyright © 2018 HYPEIT INC. Peter Rexelius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
